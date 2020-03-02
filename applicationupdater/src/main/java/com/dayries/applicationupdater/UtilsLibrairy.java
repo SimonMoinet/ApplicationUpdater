7eaa61c74fd56f1ae2cdda4cd437a757b1b8bb4d
Simon Moinet <simon@dayries.com>
2020-03-02 14:24:40 +0100

    refactor: renommage du dossier app en applicationupdater

--- applicationupdater/src/main/java/com/dayries/applicationupdater/UtilsLibrairy.java
+++ applicationupdater/src/main/java/com/dayries/applicationupdater/UtilsLibrairy.java
@@ -9,12 +9,14 @@ import androidx.core.content.FileProvider;
 import com.android.volley.BuildConfig;
 
 import java.io.File;
+import java.util.Objects;
 
 public class UtilsLibrairy {
 
     public static Uri uriFromFile(Context context, File file) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            return FileProvider.getUriForFile(context, "com.dayries.applicationupdater.fileprovider", file);
+            return FileProvider.getUriForFile(context.getApplicationContext(),
+                    "com.dayries.applicationupdater.FileProvider", file);
         } else {
             return Uri.fromFile(file);
         }
